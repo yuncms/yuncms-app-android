7e5c3f5138da325819008a98d46c5c27156686dd
xutl <xtongle@gmail.com>
2017-07-20 23:24:12 +0800

    Init

---
+++ app/src/main/java/com/tintsoft/yuncms/ui/activity/MainActivity.java
@@ -0,0 +1,10 @@
+package com.tintsoft.yuncms.ui.activity;
+
+import com.tintsoft.yuncms.base.BaseActivity;
+
+/**
+ * Created by xutongle on 2017/7/20.
+ */
+
+public class MainActivity extends BaseActivity {
+}
