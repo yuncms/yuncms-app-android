b43936b222f4cd0bb455192dd00795c3ec512da2
xuchao <85825770@qq.com>
2017-07-20 17:27:21 +0800

    init user

---
+++ app/src/main/java/com/tintsoft/yuncms/bean/Profile.java
@@ -0,0 +1,241 @@
+package com.tintsoft.yuncms.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Profile implements Serializable {
+
+    @SerializedName("user_id")
+    int id;
+
+    String nickname;
+
+    int gender;
+
+    String mobile;
+
+    @SerializedName("public_email")
+    String publicEmail;
+
+    String country;
+
+    String province;
+
+    String city;
+
+    String location;
+
+    String address;
+
+    String website;
+
+    String timezone;
+
+    String introduction;
+
+    String bio;
+
+    String birthday;
+
+    String current;
+
+    String qq;
+
+    String weibo;
+
+    String wechat;
+
+    String facebook;
+
+    String twitter;
+
+    String company;
+
+    @SerializedName("company_job")
+    String companyJob;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPublicEmail() {
+        return publicEmail;
+    }
+
+    public void setPublicEmail(String publicEmail) {
+        this.publicEmail = publicEmail;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String timezone) {
+        this.current = current;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getWeibo() {
+        return weibo;
+    }
+
+    public void setWeibo(String weibo) {
+        this.weibo = weibo;
+    }
+
+    public String getWechat() {
+        return wechat;
+    }
+
+    public void setWechat(String wechat) {
+        this.wechat = wechat;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompanyJob() {
+        return companyJob;
+    }
+
+    public void setCompanyJob(String companyJob) {
+        this.companyJob = companyJob;
+    }
+}
---
+++ app/src/main/java/com/tintsoft/yuncms/bean/User.java
@@ -0,0 +1,86 @@
+package com.tintsoft.yuncms.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+
+public class User implements Serializable {
+
+    int id;
+
+    String username;
+
+    String nickname;
+
+    String mobile;
+
+    String email;
+
+    String avatar;
+
+    @SerializedName("registration_ip")
+    String registrationIp;
+
+    @SerializedName("created_at")
+    String createdAt;
+
+    @SerializedName("updated_at")
+    String updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getRegistrationIp() {
+        return registrationIp;
+    }
+
+    public void setRegistrationIp(String registrationIp) {
+        this.registrationIp = registrationIp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
